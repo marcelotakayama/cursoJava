774d1aa5d3a15651c5eb70b01bd4f55e5d5cd9ec
marcelotakayama <marcelo.takayama00@gmail.com>
2020-05-23 20:43:35 -0300

    Introdução a inferência nas variáveis do Java

---
+++ src/fundamentos/Inferencia.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class Inferencia {
+    public static void main(String[] args) {
+
+        double a = 4.5;
+        System.out.println(a);
+
+        double b = 4.5;
+        System.out.println(b);
+
+        String c = "Oi";
+        System.out.println(c);
+
+        c = "Outro Oi";
+        System.out.println(c);
+    }
+}

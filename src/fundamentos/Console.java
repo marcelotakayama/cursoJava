e9a37435876e1cd3f39c2296bc874d3af5459cfe
marcelotakayama <marcelo.takayama00@gmail.com>
2020-05-24 18:01:17 -0300

    Incrementando os principais conceitos do console do Java

--- src/fundamentos/Console.java
+++ src/fundamentos/Console.java
@@ -1,5 +1,7 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class Console {
     public static void main(String[] args) {
         System.out.print("Bom");
@@ -7,5 +9,24 @@ public class Console {
 
         System.out.println("Bom");
         System.out.println("Dia");
+
+        System.out.printf("Megasena: %d %d %d %d %d %d \n", 1, 2, 3, 4, 5, 6);
+        System.out.printf("Salário: %.1f%n", 1234.5678);
+        System.out.printf("Nome: %s%n", "Marcelo");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = entrada.nextLine();
+
+        System.out.println("Digite a sua idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.println("\nNome: " + nome + " " + sobrenome + "\nIdade: " + idade);
+        System.out.printf("%s %s tem %d anos.", nome, sobrenome, idade);
+        entrada.close();
     }
 }
